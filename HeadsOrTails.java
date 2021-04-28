b0c179694bf4f1bf388ca41be6ddc736f57c0bf0
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-28 01:21:25 -0700

    :books: Chore: Add comments to HeadsOrTails.java:

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -11,8 +11,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -23,20 +21,21 @@ import java.util.Random;
 public class HeadsOrTails extends Application
 {
 	
+	/* Private Fields */
 	private Random random = new Random((new Random()).nextLong());
-	
 	private ImageView coin;
 	
+	/* Start of JavaFX Application */
 	public void start(Stage primaryStage)
 	{
 		
+		/* Set up coin image view */
 		coin = new ImageView();
-		
 		this.coin.setFitHeight(200);
 		this.coin.setFitWidth(200);
 		
+		/* Set up button */
 		Button btn = new Button("Toss");
-		
 		btn.setOnAction((event) ->
 		{
 			
@@ -51,24 +50,27 @@ public class HeadsOrTails extends Application
 			
 		});
 		
+		/* Set up the positioning of the VBox to add to scene and stage */
 		VBox box = new VBox(10, this.coin, btn);
-		
 		box.setAlignment(Pos.CENTER);
 		box.setPadding(new Insets(10, 10, 10, 10));
 		
+		/* Add to scene and add to stage */
 		Scene scene = new Scene(box);
-		
 		primaryStage.setScene(scene);
 		
+		/* Setup stage (window) and display */
 		primaryStage.setTitle("Heads Or Tails?");
 		primaryStage.setResizable(false);
 		primaryStage.show();
 		
 	}
 	
+	/* Driver function */
 	public static void main(String[] args)
 	{
 		
+		/* Start JavaFX Application */
 		launch(args);
 		
 	}
